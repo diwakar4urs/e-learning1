7f9a31cc99c3fbaecc34f5df7cbb214f7fbf7f22
diwakar4urs <diwakar4urs@gmail.com>
2018-09-19 18:15:49 +0530

    commit
    commit

---
+++ local/resources/views/layouts/site-navigation.blade.php
@@ -0,0 +1,8 @@
+<div class="menus">
+			<ul>
+				<li><i class="icon-education-cap"></i> {{ getPhrase('educate')}}</li>
+				<li><i class="icon-education-cap"></i> {{ getPhrase('enlight')}}</li>
+				<li><i class="icon-education-cap"></i> {{ getPhrase('enforce')}}</li>
+				 
+			</ul>
+</div>
